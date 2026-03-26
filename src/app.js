42a065813aa12570b6f9eecdd1bff7ac8449d4c3
Chris M <chrism@mindarc.com.au>
2026-03-26 18:17:35 +1000

    Adding arcjet security

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import logger from '#config/logger.js';
-import helmet from "helmet";
+import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(cookieParser());
+app.use(securityMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined', { stream: { write: message => logger.info(message.trim()) } }));
